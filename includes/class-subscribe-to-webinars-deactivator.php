8a798cd7fe8d580d9398d5268cbc1f878a7127fa
kgkrishna <kgkrishna@visualbi.com>
2019-11-20 12:03:31 +0530

    Initia Dev Commit

---
+++ includes/class-subscribe-to-webinars-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://visualbi.com
+ * @since      1.0.0
+ *
+ * @package    Subscribe_To_Webinars
+ * @subpackage Subscribe_To_Webinars/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Subscribe_To_Webinars
+ * @subpackage Subscribe_To_Webinars/includes
+ * @author     Balasubramaniyan M and K Gopal Krishna <website@visualbi.com>
+ */
+class Subscribe_To_Webinars_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
